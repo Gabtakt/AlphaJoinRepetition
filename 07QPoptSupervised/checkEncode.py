fb70ddf7b040c9a0510342c51aa5a704c14b5a4b
lbc <bench_lian@hust.edu.cn>
2021-03-29 14:26:48 +0800

    add test file

--- 07QPoptSupervised/checkEncode.py
+++ 07QPoptSupervised/checkEncode.py
@@ -16,12 +16,14 @@ def checkEncode():
     encodeDict2 = eval(a)
     f.close()
 
+    # 检查one-hot编码有效部分跟histogram编码有效部分是否相同
     for K,V in encodeDict1.items():
         V2 = encodeDict2[K]
         for i in range(0,len(V)):
             if V[i] != 0:
-                print(V[i],V2[i])
                 assert(V2[i] != 0)
+            if V2[i] != 0:
+                assert(V[i] != 0)
 
     # print(encodeDict1)
     # print(encodeDict2)
