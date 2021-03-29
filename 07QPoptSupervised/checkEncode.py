e61d317ef9b044b29baa9ddb6eca27351ffd4078
lbc <bench_lian@hust.edu.cn>
2021-03-29 14:20:51 +0800

    add test file

--- 07QPoptSupervised/checkEncode.py
+++ 07QPoptSupervised/checkEncode.py
@@ -16,5 +16,11 @@ def checkEncode():
     encodeDict2 = eval(a)
     f.close()
 
-    print(encodeDict1)
-    print(encodeDict2)
+    for K,V in encodeDict1.items():
+        V2 = encodeDict2[K]
+        for i = 0 in range(0,len(V)):
+            if V[i] != 0:
+                assert(V2[i] != 0)
+
+    # print(encodeDict1)
+    # print(encodeDict2)
